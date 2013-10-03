45ac3d1c729c62bb280ef2761591746b338d576f
Michael Revell <mikearevell@gmail.com>
2013-10-03 00:56:34 -0700

    boom

--- Tap Me/ViewController.m
+++ Tap Me/ViewController.m
@@ -18,12 +18,56 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self setupGame];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setupGame {
+    seconds = 5;
+    count = 0;
+    
+    timerLabel.text = [NSString stringWithFormat:@"Time: %i", seconds];
+    scoreLabel.text = [NSString stringWithFormat:@"Score\n%i", count];
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                             target:self
+                                           selector:@selector(subtractTime)
+                                           userInfo:nil
+                                            repeats:YES];
+}
+
+- (void)subtractTime {
+    // 1
+    seconds--;
+    timerLabel.text = [NSString stringWithFormat:@"Time: %i",seconds];
+    
+    // 2
+    if (seconds == 0) {
+        [timer invalidate];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Time is up!"
+                                                        message:[NSString stringWithFormat:@"You scored %i points", count]
+                                                       delegate:self
+                                              cancelButtonTitle:@"Play Again"
+                                              otherButtonTitles:nil];
+        
+        [alert show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    [self setupGame];
+}
+
+- (IBAction)buttonPressed {
+    count++;
+    scoreLabel.text = [NSString stringWithFormat:@"Score\n%i", count];
+    
 }
 
 @end
